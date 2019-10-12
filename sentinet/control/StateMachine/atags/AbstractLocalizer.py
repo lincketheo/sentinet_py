57e50a0b80be23201bec2bfa7868cc52e3bcc532
Sidhant Puntambekar <35539945+SidhantPuntambekar@users.noreply.github.com>
2019-10-11 18:14:06 -0600

    WIP abstract class for localizer

---
+++ sentinet/control/StateMachine/atags/AbstractLocalizer.py
@@ -0,0 +1,18 @@
+from abc, import ABC, abstractmethod
+
+class AbstractLocalizer(ABC)
+    def __init__(self,value):
+        self.value = value
+        super().__init__()
+
+        @abstractmethod
+        def position(self):
+            pass
+
+        @abstractmethod
+        def velocity(self):
+            pass
+
+        @abstractmethod
+        def acceleration(self):
+            pass
