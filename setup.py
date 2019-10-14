eb05846f607079d4efb9235a72b77eb4a13fa3a7
tlincke125 <thli2739@colorado.edu>
2019-10-14 10:24:15 -0600

    added simple documentation

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
+#!/usr/bin/env python3
 
 from setuptools import setup, find_packages
 
 setup( 
         name='SentiNet',
         version='1.0',
+        description="A Python Version of Sentinet",
+        author="Trace Valade",
+        author_email="trace.valade@colorado.edu",
         packages=[package for package in find_packages() if package.startswith('sentinet')],
+        install_requires=[
+            'numpy',
+            'pyzmq' ]
        )
