c0eb8a3fa3b02a54a344a194cef7eb90b9f5eed1
tlincke125 <thli2739@colorado.edu>
2019-10-26 01:25:49 -0600

    updated message

--- sentinet/core/messages/Message.py
+++ sentinet/core/messages/Message.py
@@ -1,258 +1,251 @@
 import sys
 from struct import pack
 from sentinet.core.messages.MessageKeys import *
+import copy
 
-
+##
+# @brief A Data Message that holds Data
 class Data_Message:
 
-    def __init__(self, incomming_message=0): 
+    ##
+    # @brief Default constructor
+    #
+    # @return A new Data message
+    def __init__(self): 
+        self.header = copy.deepcopy(default_header)
+        # + 1 for null terminator
+        self.header_size = self.header["FIELDS"].index + self.header["FIELDS"].data_size + 1
+        self.message = bytearray(self.header_size) 
+        # Serialize header, if an error occurs here
+        # it's not a runtime issue, its a versioning one
+        if not self.serialize_header():
+            print("Exiting")
+            sys.exit(1)
+        # For faster retrivals of data, 
+        # store the indexes of each element in indices
+        self.indices = []
 
-        # Recieve the default header for backwards compatability sake
-        self.header = default_header
-
-        # Get the size of the header (TODO - this is still to explicit)
-        self.header_size = self.header["FIELDS"].index + self.header["FIELDS"].data_size
-
-        # Recieving a message
-        if incomming_message is not 0:
-            self.message = incomming_message
-
-            # update values by reading elements from within the buffered message
-            for i in self.header.values():
-                i.data = self.get_header_element_raw(i.data_size, i.index) 
-
-        # Not recieving a message, so we need to make a new message
-        else:
-            self.message = bytearray(self.header_size) 
-            self.message.append(0)
-            if not self.serialize_header():
-                print("Exiting")
-                sys.exit(1)
-
-
-    # Emplaces all header elements in self.header
-    # into the message buffer
+    ##
+    # @brief Serializes data in self.header into header buffer
+    #
+    # @return Status
     def serialize_header(self):
         i = 0 
         # For every data element, serialize it into the header
         for element in self.header.values():
-            # Check for indexes and make sure they match
             if i != element.index:
                 print("Error - invalid header index")
                 return False
-
-            # Otherwise, serialize the data element
             self.message[i:i+element.data_size] = element.serialize()
-
-            # Incriment index by data size so there is no structure padding
             i += element.data_size
-
         # Add a null terminator
         self.message[i] = 0
         return True
-         
-    # Returns element that is in the header
-    def get_header_element(self, element):
-        # This checks for the map not the message
-        # bytearray
-        if element in self.header:
-            return self.header[element].data
-        else:
-            print("Invalid elemet")
 
-    # Returns element that is in the message header
+    ##
+    # @brief Gets data from the header buffer
+    #
+    # @return Status
+    def serialize_from_header(self):
+        i = 0 
+        for element in self.header.values():
+            if i != element.index:
+                print("Error - invalid header index")
+                return False
+            element.data = int.from_bytes(self.message[i:i+element.data_size], byteorder = ENDIAN, signed = element.signed)
+            i += element.data_size
+        return True
+     
+    ##
+    # @brief Retrieves a value from the header buffer
+    #
+    # @param size The size of the value
+    # @param index The index to get
+    #
+    # @return The value at that index
     def get_header_element_raw(self, size, index):
-        # Utilizing the fact that all our header elements can be expressed as 
-        # unsigned ints of arbitrary sizes - a little too explicit, may want to change
+        if index + size > self.header_size:
+            print("Error, invalid range")
+            return None
         return int.from_bytes(self.message[index:index+size], byteorder = ENDIAN, signed=False)
 
-    # Serialize a data element (not in the header that is)
-    def serialize_data(self, data, bytes_size, type_code):
-        # Check data type, otherwise, we
-        # simply represent it as an int
-        # TODO add more cases here - maybe a switch table
-        if(type(data) == str):
-
-            # The byte size should always be 1 more than the size of the string 
-            # To account for c style null terminator
-            if(bytes_size != len(data) + 1):
-                print("ERROR")
-                return 1
-
-            # Add a string null terminator for C 
-            # and turn it into bytes
-            data = data.encode('utf-8') + b"\x00"
-
-        # simply convert the data to bytes
-        # TODO im pretty sure this is fine for floats, but I'd have to check
-        elif(type(data) == int):
-            # Convert data to bytes
-            data = data.to_bytes(bytes_size, byteorder = ENDIAN, signed = False)
-        elif(type(data) == float):
-            # Convert data to bytes
-            data = pack('f', data)
-
-        # Append data
-
-        # First, the message size (1 byte)
+    ##
+    # @brief Pushes a new data element to the end of data message (see set_data for alternative)
+    #
+    # @param data The "raw" data to serializable by the codes defined in MEssageKeys.py
+    # @param bytes_size The size of the data - does some checks to make sure this is right
+    # @param type_code The type code, see MessageKeys.py
+    #
+    # @return Status of serialization
+    def push_data(self, data, bytes_size, type_code):
+        # Do the preliminary checks on the data
+        if type_code in serialize_funcs:
+            if serialize_funcs[type_code][0](data, bytes_size):
+                data = serialize_funcs[type_code][1](data)
+            else:
+                print("Invalid data type for declared type code")
+                return False
+        else:
+            print("Invalid type code")
+            return False
+        
+        # Serialize the data to the end
+        self.indices.append(self.header["BYTE_LENGTH"].data) # Change this
         self.message.append(bytes_size)
-
-        # Second the message type code in utf (1 byte)
         self.message.append(ord(type_code))
-
-        # Third, the data
         self.message.extend(data)
-
-        # Fourth the null terminator
         self.message.append(0)
-
-        # Update header fields and byte length
         self.header["FIELDS"].data += 1
         self.header["BYTE_LENGTH"].data += 3 + bytes_size
 
-    def set_data(self, data: bytes, byte_size: int, type_code, byte_index):
-        if self.header["BYTE_LENGTH"].data < byte_index + byte_size + 3:
-            print("Index to high, pushing to the top of stack")
-            self.serialize_data(data, byte_size, type_code)
-            return True
-        # Itterate through the message
-        print(self.message)
-        self.message[byte_index] = byte_size
-        self.message[byte_index + 1] = ord(type_code)
-        self.message[byte_index + 2:byte_index + byte_size + 2] = data
-        self.message[byte_index + byte_size + 2] = 0
-         
+    ##
+    # @brief Sets data within a message, ideal for when you know the attributes of the message
+    #
+    # @note This function sets a new data element and updates indices
+    #
+    # @param data The data to serialize
+    # @param bytes_size The size of the element
+    # @param type_code The type code
+    # @param index The index to serialize into
+    #
+    # @return Status of serialization
+    def set_data(self, data, bytes_size, type_code, index):
+        if index >= self.header["FIELDS"].data or bytes_size > 255:
+            print("Invalid index")
+            return False
+        if type_code in serialize_funcs:
+            if serialize_funcs[type_code][0](data, bytes_size):
+                data = serialize_funcs[type_code][1](data)
+            else:
+                print("Invalid data type for declared type code")
+                return False
+        else:
+            print("Invalid type code")
+            return False
+
+        # Index of data
+        i = self.indices[index]
+        print(type(self.message[i:i+1]))
+        # Size of data at index
+        size = int.from_bytes(self.message[i:i+1], byteorder = ENDIAN)
+
+        # Optimal operations for three cases
+        if size == bytes_size:
+            self.message[i] = bytes_size
+            self.message[i + 1] = ord(type_code)
+            self.message[i + 2:i + 2 + bytes_size] = data
+
+        elif size > bytes_size:
+            self.message[i] = bytes_size
+            self.message[i + 1] = ord(type_code)
+            self.message[i + 2:i + 2 + bytes_size] = data
+            del self.message[i + 2 + bytes_size: i + 2 + size]
+            diff = size - bytes_size
+            for i in range(len(self.indices) - index - 1):
+                self.indices[i + index + 1] -= diff
+
+        elif size < bytes_size:
+            diff = bytes_size - size
+            self.message[i] = bytes_size
+            self.message[i + 1] = ord(type_code)
+            # Figured out that python automatically
+            # allocates extra space for data if data
+            # size is larger
+            self.message[i + 2:i + 2 + size] = data
+            for i in range(len(self.indices) - index - 1):
+                self.indices[i + index + 1] += diff
+
+        
+        self.header["BYTE_LENGTH"].data += (bytes_size - size)
+
+    ##
+    # @brief Gets byte data at a specified index
+    #
+    # @param index The index to retriev
+    #
+    # @return The data
+    def get_data(self, index):
+        if index >= self.header["FIELDS"].data:
+            print("Invalid index")
+            return None
+        index = self.indices[index]
+        size = self.message[index]
+        return self.message[index + 2:index + 2 + size]
+
+    ##
+    # @brief Parse from a message and update self attributes
+    #
+    # @note This shouldn't be used real time. See next method
+    #
+    # @param message The incomming message
+    #
+    # @return Status of parse
+    def parse_from_message(self, message):
+        self.message = message
+        self.indices = []
+        
+        i = 13
+        if self.message[0] > 1:
+            return 1
+        if self.message[12] != 0:
+            return 2
+        bytes_size = int.from_bytes(self.message[self.header["BYTE_LENGTH"].index: \
+                self.header["BYTE_LENGTH"].index + self.header["BYTE_LENGTH"].data_size], byteorder = ENDIAN)
+        if bytes_size != len(self.message):
+            return 3
+        while i < bytes_size:
+            if self.message[i] == 0:
+                return 4
+            size = self.message[i] 
+            if 3 + size + i > bytes_size:
+                return 5
+            self.indices.append(i)
+            i += size + 2
+            if self.message[i] != 0:
+                return 6
+            i += 1
+        self.serialize_from_header()
+        return True
+                        
+    ##
+    # @brief Parse a message that has the same structure as this one
+    #
+    # @param message The incomming message
+    #
+    # @return The status of parse
+    def parse_from_similar_message(self, message):
+        print(len(message))
+        print(self.header["BYTE_LENGTH"].data)
+        if self.header["BYTE_LENGTH"].data != len(message):
+            return False
+        self.message = message
+        for i in range(len(self.indices)):
+            if self.message[self.indices[i] - 1] != 0:
+                return False
+        return True
+        
+
 # A ping message is a lot easier to work with
 # Because it's one big header
 class Ping_Message:
+    def __init__(self):
+        self.header_size = 15
+        self.message = bytearray(self.header_size)
 
-    def __init__(self, incomming_message = 0):
+    def set_protocol(self, value):
+        self.message[0] = value
 
-        # Set it as default to have a standard even if 
-        # we simply recieve the message
-        self.header = default_ping
-        self.header_size = default_ping["EXCESS"].index + default_ping["EXCESS"].data_size + 1
+    def set_opcode(self, value):
+        self.message[1] = value
 
-        # If we have no incomming message, create a new packet
-        # This could be done in a ternerary, but there may be more we 
-        # want to do so I'm leaving it as if else
-        if incomming_message == 0:
-            self.message = bytearray(self.header_size)
+    def set_type(self, value):
+        self.message[2] = value
 
-        else: 
-            # Check the incomming message - see below TODO
-            if self.header_size != len(incomming_message):
-                print("Error incomming message size does not match a standard ping message")
+    def set_code(self, value):
+        self.message[3:5] = value.to_bytes(2, byteorder = ENDIAN, signed = False)
 
-            # Copy the message over
-            self.message = incomming_message
+    def set_checksum(self, value):
+        self.message[5:7] = value.to_bytes(2, byteorder = ENDIAN, signed = False)
 
-            # Then itterate through to update the dict values
-            for i in self.header.values():
-                i.data = self.get_header_element_raw(i.data_size, i.index) 
-
-            # TODO - put all incomming message checks in their
-            # own method called validate incomming message
-            # Right now, I'm just serializing and expecting
-            # nothing to change
-            if not self.serialize():
-                print("Exiting")
-                sys.exit(1)
-
-            if self.message != incomming_message:
-                print("Error parsing incomming message")
-
-    # Serialize the data within our message
-    def serialize(self):
-        i = 0
-
-        # Itterate through all items in the dict and update byte array
-        for element in self.header.values():
-
-            # Check for index matching
-            if i != element.index:
-                print("Error serializing ping, indexes are off")
-                return False
-
-            # Otherwise, copy data into the buffer and increment the index
-            self.message[i:i+element.data_size] = element.serialize()
-            i += element.data_size
-
-        # A null terminator
-        self.message[i] = 0
-        return True
-
-    # Gets element at a specified key
-    def get_element(self, element):
-        if element in self.header:
-            return self.header[element].data
-        else:
-            print("Invalid elemet")
-
-    # Gets element at specified index and size in byte form
-    def get_element_raw(self, size, index):
-        return int.from_bytes(self.message[index:index+size], byteorder = ENDIAN, signed=False)
-
-    # Sets data element in dict (not message)
-    def set_element(self, element, value):
-        if element in self.header:
-            self.header[element].data = value
-        else:
-            print("Invalid elemet")
-
-    
-
-if __name__ == '__main__':
-
-    # Example:
-    dm1 = Data_Message()
-
-    # Serialize some data
-    dm1.serialize_data("Hello", 6, STRING)
-    dm1.serialize_data(5.9999123123, 4, FLOAT)
-
-    # Shouldn't serialize because the length does not
-    # account for null terminator
-    print("Expecting ERROR: ")
-    dm1.serialize_data("invalid", len("invalid"), STRING)
-
-    # Should serialize
-    dm1.serialize_data("valid", len("valid") + 1, STRING)
-    
-    # Update the header
-    dm1.serialize_header()
-
-    # Print some data
-    print(dm1.message)
-    print("Header byte length: " + str(dm1.header["BYTE_LENGTH"].data))
-    print("Actual byte length: " + str(len(dm1.message)))
-
-    # Note that get_header_element_raw should never be used outside,
-    # I'm just using it to demonstrate
-    print("Byte length in mes: " + str(dm1.get_header_element_raw(4, 6)))
-
-    dm2 = Data_Message(dm1.message)
-    dm2.serialize_header()
-
-    # Print some data
-
-    print(dm2.message)
-    print("Header byte length: " + str(dm2.header["BYTE_LENGTH"].data))
-    print("Actual byte length: " + str(len(dm2.message)))
-
-    # Note that get_header_element_raw should never be used outside,
-    # I'm just using it to demonstrate
-    print("Byte length in mes: " + str(dm2.get_header_element_raw(4, 6)))
-
-
-    pm1 = Ping_Message()
-    pm1.set_element("TYPE", 0x2)
-    pm1.set_element("CHECKSUM", 0x3)
-    pm1.set_element("CODE", 1)
-    pm1.set_element("EXCESS", 123123123)
-    pm1.serialize()
-    print(pm1.message)
-    print("123123123 = " + str(pm1.get_element_raw(8, 7)))
-    print("3 = " + str(pm1.get_element_raw(2, 5)))
-    print("1 = " + str(pm1.get_element_raw(2, 3)))
-    print("2 = " + str(pm1.get_element_raw(1, 2)))
+    def set_excess(self, value):
+        self.message[7:15] = value.to_bytes(8, byteorder = ENDIAN, signed = False)
--- sentinet/core/messages/MessageKeys.py
+++ sentinet/core/messages/MessageKeys.py
@@ -1,3 +1,4 @@
+import struct
 # A place for message keys
 
 
@@ -23,12 +24,67 @@ UINT16 = 'q'
 INT32 = 'i' 
 INT = INT32
 UINT32 = 'u' 
-INT64 = 't' 
+INT64 = 'x'
+UINT64 = 't' 
 FLOAT = 'f' 
 DOUBLE = 'd' 
 STRING = 's' 
 OBJECT = 'o' 
 ARRAY = 'a' 
+
+
+# I had a lot of fun with lambdas
+# Each element has an assert and a serialize callback
+# 1st lambda checks that the data is valid, second returns a byte version of that data
+serialize_funcs = {
+        INVALID :   ((lambda val, byte_length: False), 
+                    (lambda val: False)),
+
+        CHAR :      ((lambda val, byte_length: byte_length == 1 and type(val) == str and len(val) == 1), 
+                    (lambda val: ord(val))),
+
+        BYTE_TYPE : ((lambda val, byte_length: byte_length == 1 and type(val) == bytes and len(val) == 1), 
+                    (lambda val: val)),
+
+        BOOL :      ((lambda val, byte_length: byte_length == 1 and type(val) == bool), 
+                    (lambda val: 1 if val else 0)),
+
+        INT8 :      ((lambda val, byte_length: byte_length == 1 and type(val) == int and val < 128 and val > -129),
+                    (lambda val : val.to_bytes(1, byteorder = ENDIAN, signed = True))),
+
+        UINT8 :     ((lambda val, byte_length: byte_length == 1 and type(val) == int and val < 256),
+                    (lambda val : val.to_bytes(1, byteorder = ENDIAN, signed = False))),
+
+        UINT16 :    ((lambda val, byte_length: byte_length == 2 and type(val) == int and val < 65536),
+                    (lambda val : val.to_bytes(2, byteorder = ENDIAN, signed = False))),
+
+        INT32 :     ((lambda val, byte_length: byte_length == 4 and type(val) == int and val < 2147483648 and val > -2147483649),
+                    (lambda val : val.to_bytes(4, byteorder = ENDIAN, signed = True))),
+
+        UINT32 :    ((lambda val, byte_length: byte_length == 4 and type(val) == int and val < 4294967296),
+                    (lambda val : val.to_bytes(4, byteorder = ENDIAN, signed = False))),
+
+        INT64 :     ((lambda val, byte_length: byte_length == 8 and type(val) == int and val < 9223372036854775808 and val > -9223372036854775809), 
+                    (lambda val : val.to_bytes(8, byteorder = ENDIAN, signed = True))),
+
+        UINT64 :    ((lambda val, byte_length: byte_length == 8 and type(val) == int and val < 18446744073709551616), 
+                    (lambda val : val.to_bytes(8, byteorder = ENDIAN, signed = False))),
+
+        FLOAT :     ((lambda val, byte_length: byte_length == 4 and type(val) == float), 
+                    (lambda val : struct.pack("!f", val))),
+
+        DOUBLE :    ((lambda val, byte_length: byte_length == 8 and type(val) == float), 
+                    (lambda val : struct.pack("!d", val))),
+
+        STRING :    ((lambda val, byte_length: type(val) == str and len(val) + 1 == byte_length),
+                    (lambda val : val.encode('utf-8') + b'\x00')),
+
+        OBJECT :    ((lambda val, byte_length: False),
+                    (lambda val : print("Idiot, don't serielaize objects"))),
+
+        ARRAY :     ((lambda val, byte_length: type(val) == bytearray and len(val) == byte_length),
+                    (lambda val : val))}
+
 
 class DataElement:
     def __init__(self, value, data_size, index, signed):
