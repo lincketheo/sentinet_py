ae317a836af1f5d71ebbc838c425121c11cfed83
Sidhant Puntambekar <35539945+SidhantPuntambekar@users.noreply.github.com>
2019-11-08 18:04:39 -0700

    Update AbstractLocalizer.py

--- sentinet/control/StateMachine/atags/AbstractLocalizer.py
+++ sentinet/control/StateMachine/atags/AbstractLocalizer.py
@@ -1,14 +1,26 @@
-
 from abc, import ABC, abstractmethod
+from multiprocessing import Pipe
+from sentinet.control.Localizer.Localizer_Base import LocalizerBase, SensorBase
+from threading import Lock
+from sentinet.curmt import KermitControlModule
+import numpy as np
 
-class AbstractLocalizer(ABC)
-    def __init__(self,value):
-        self.value = value
-        super().__init__()
+class AprilTagsLocalizer(LocalizerBase)
 
+    def __init__(self,pipe,sensors):
+        super().__init__(pipe,sensors):
+            if None in self.position:
+			    self.position.fill(0.0)
+		    if None in self.velocity:
+			    self.velocity.fill(0.0)
+		    if None in self.ang_position:
+			    self.ang_position.fill(0.0)
+            if None in self.ang_velocity:
+                self.ang_velocity.fill(0.0)
+ 
         @abstractmethod
         def position(self):
-            pass
+            
 
         @abstractmethod
         def velocity(self):
@@ -18,3 +30,19 @@ class AbstractLocalizer(ABC)
         def acceleration(self):
             pass
 
+class IMUSensorBase(SensorBase):
+    float angularAccel
+    float linearAccel
+    float posX
+    float posY
+    float pozZ
+    
+    def __init__(self):
+        super().__init__(pipe,sensor):
+            angularAccel. 
+            linearAccel. 
+            posX. 
+            posY.
+            pozZ.
+        
+        
